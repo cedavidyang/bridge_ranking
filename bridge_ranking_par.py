03ef898d41b0e1a5afa61c1f48987283b3cb1970
cedavidyang <ynyang1988@yahoo.com>
2015-07-04 14:08:07 -0400

    change simulation number to 100 for test

--- bridge_ranking_par.py
+++ bridge_ranking_par.py
@@ -57,7 +57,7 @@ t = 50
 # get current cs distribution
 cs_dist = pytraffic.condition_distribution(t, bridge_db, pmatrix)
 # number of smps
-nsmp = int(1e3)
+nsmp = int(100)
 # initial capacity without failed bridges
 all_capacity = np.zeros(nlink)
 for link, link_indx in graph0.indlinks.iteritems():
