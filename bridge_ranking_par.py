da51e3f7da1e254ff8f1d355e42be5719305fc36
cedavidyang <ynyang1988@yahoo.com>
2015-07-10 18:42:49 -0400

    test year10, nsmp 10 and process 10 with the same bridge

--- bridge_ranking_par.py
+++ bridge_ranking_par.py
@@ -59,7 +59,7 @@ if __name__ == '__main__':
     try:
         pool = Pool(processes = 10)
         #res = pool.map_async(loop_over_bridges, np.arange(bridge_db.shape[0])).get(0xFFFFFFFF)
-        res = pool.map_async(loop_over_bridges, np.arange(10)).get(0xFFFFFFFF)
+        res = pool.map_async(loop_over_bridges, np.zeros(10)).get(0xFFFFFFFF)
         #results = [pool.apply_async(loop_over_bridges, (b,)) for b in np.arange(30)]
 
         #res = [r.get() for r in results]
