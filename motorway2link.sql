af0b863e37a07f6a83ec65ff7cd08d66c2e113e7
cedavidyang <ynyang1988@yahoo.com>
2015-07-11 16:45:16 -0400

    add BEGIN and COMMIT

--- motorway2link.sql
+++ motorway2link.sql
@@ -1,4 +1,6 @@
-﻿CREATE TABLE network.LA_links
+﻿SET CLIENT_ENCODING TO UTF8;
+BEGIN;
+CREATE TABLE network.LA_links
 AS (
 select
     (select distinct nodes.gid from network.LA_nodes nodes where nodes.geom = ST_StartPoint(ST_LineMerge(motorway.geom))) as fromid,
@@ -13,8 +15,8 @@ select
 from network.LA_motorway motorway
 );
 ALTER TABLE network.LA_links ADD COLUMN type character varying(15) DEFAULT 'HIGHWAY';
-ALTER TABLE network.LA_links ADD COLUMN capacity double precision DEFAULT 4500.0;
+ALTER TABLE network.LA_links ADD COLUMN capacity double precision DEFAULT 8000.0;
 ALTER TABLE network.LA_links ADD COLUMN freespeed double precision DEFAULT 30.0;
 ALTER TABLE network.LA_links ADD COLUMN gid SERIAL NOT NULL;
 ALTER TABLE network.LA_links ADD COLUMN id SERIAL NOT NULL;
-select * from network.LA_links;
+COMMIT;
