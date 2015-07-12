ea744ad1ab3cb4b2ea98afcb83ae00a6132f327e
cedavidyang <ynyang1988@yahoo.com>
2015-07-11 21:53:31 -0400

    test year10, nsmp1000 and process10

--- bridge_ranking_par.py
+++ bridge_ranking_par.py
@@ -35,7 +35,7 @@ t = 10
 cs_dist = pytraffic.condition_distribution(t, bridge_db, pmatrix)
 cost0 = social_cost(delay0, distance0, t)
 # number of smps
-nsmp = int(10)
+nsmp = int(1000)
 
 #def loop_over_bridges(bridge_indx, bookkeeping):
 def loop_over_bridges(bridge_indx):
@@ -58,8 +58,8 @@ if __name__ == '__main__':
     print 'CALC: Parallel version'
     try:
         pool = Pool(processes = 10)
-        #res = pool.map_async(loop_over_bridges, np.arange(bridge_db.shape[0])).get(0xFFFFFFFF)
-        res = pool.map_async(loop_over_bridges, np.arange(10)).get(0xFFFFFFFF)
+        res = pool.map_async(loop_over_bridges, np.arange(bridge_db.shape[0])).get(0xFFFFFFFF)
+        #res = pool.map_async(loop_over_bridges, np.arange(10)).get(0xFFFFFFFF)
         #res = pool.map_async(loop_over_bridges, np.zeros(1, dtype=int)).get(0xFFFFFFFF)
         #results = [pool.apply_async(loop_over_bridges, (b,)) for b in np.arange(30)]
 
