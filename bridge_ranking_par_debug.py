672d00bdbed6b4cadb3cb3e0e5196059e790ef8b
David Yang <ynyang1988@yahoo.com>
2017-06-28 19:56:58 -0400

    save some effort to generate metadata

---
+++ bridge_ranking_par_debug.py
@@ -0,0 +1,107 @@
+"""
+Created on Thu Jun 04 12:19:41 2015
+
+@author: cedavidyang
+"""
+__author__ = 'cedavidyang'
+
+import os
+import sys
+
+import numpy as np
+import pyNBI.traffic as pytraffic
+from pyNBI.risk import social_cost
+
+from multiprocessing import Pool, Manager, freeze_support, Queue, Process
+import Queue as queue
+import itertools
+
+import time
+import datetime
+import shelve
+
+# global variables for parallel computing... stupid multiprocessing in Python
+
+# to restore workspace import global variables
+filename = os.path.join(os.path.abspath('./'), 'Data', 'Python', 'metadata.out')
+my_shelf = shelve.open(filename, 'r')
+for key in my_shelf:
+    globals()[key]=my_shelf[key]
+my_shelf.close()
+
+# time of interest
+t = 10
+# get current cs distribution and socialcost0
+cs_dist = pytraffic.condition_distribution(t, bridge_db, pmatrix)
+cost0 = social_cost(delay0, distance0, t)
+# number of smps
+nsmp = int(2)
+
+#def loop_over_bridges(bridge_indx, bookkeeping):
+def loop_over_bridges(bridge_indx):
+    indx, smp = pytraffic.delay_samples(nsmp, graph0, cost0, all_capacity, t, bridge_indx,
+            bridge_db, cs_dist, cap_drop_array, theta, delaytype,
+            correlation=norm_cov, nataf=nataf, corrcoef=0., x0=res0[0], bookkeeping={})
+
+    return indx, smp
+
+def tmpfunc(bridge_indx,q):
+    indx,smp =  loop_over_bridges(bridge_indx)
+    q.put((indx,smp))
+
+if __name__ == '__main__':
+    freeze_support()
+    nprocess = 2
+    #manager = Manager()
+    #bookkeeping = manager.dict(bookkeeping)
+
+    start_delta_time = time.time()
+    print 'CALC: Parallel version'
+    try:
+        pool = Pool(processes = nprocess)
+        #res = pool.map_async(loop_over_bridges, np.arange(bridge_db.shape[0])).get(0xFFFFFFFF)
+        #res = pool.map_async(loop_over_bridges, np.arange(10)).get(0xFFFFFFFF)
+        #res = pool.map_async(loop_over_bridges, np.zeros(nprocess, dtype=int)).get(0xFFFFFFFF)
+        #results = [pool.apply_async(loop_over_bridges, (b,)) for b in np.arange(30)]
+
+        #res = [r.get() for r in results]
+        #res = map(loop_over_bridges, np.arange(1))
+        #res = pool.map_async(loop_over_bridges,
+                #itertools.izip(itertools.repeat(nsmp), itertools.repeat(graph0), itertools.repeat(cost0),
+                    #itertools.repeat(all_capacity), np.arange(bridge_db.shape[0]), itertools.repeat(bridge_db),
+                    #itertools.repeat(cs_dist), itertools.repeat(cap_drop_array), itertools.repeat(theta),
+                    #itertools.repeat(delaytype), itertools.repeat(correlation), itertools.repeat(nataf),
+                    #itertools.repeat(bookkeeping))).get(0xFFFF)
+        #res = pool.map_async(tmpfunc,itertools.izip(np.arange(bridge_db.shape[0]), itertools.repeat(bookkeeping))).get(0xFFFF)
+        pool.close()
+        pool.join()
+
+        #q = Queue()
+        #for bridge_indx in np.zeros(10, dtype=int):
+            #p = Process(target=tmpfunc, args=(bridge_indx,q))
+            #p.start()
+        #for bridge_indx in np.zeros(10, dtype=int):
+            #p.join()
+        #res = []
+        #while True:
+            #try:
+                #res.append(q.get(timeout=10))
+            #except queue.Empty:
+                #break
+
+    except KeyboardInterrupt:
+        print "Caught KeyboardInterrupt, terminating workers"
+        pool.terminate()
+        pool.join()
+        sys.exit(1)
+    delta_time = time.time() - start_delta_time
+    print 'DONE',str(datetime.timedelta(seconds=delta_time))
+
+    start_delta_time = time.time()
+    print 'CALC: Series version'
+    res = map(loop_over_bridges, np.arange(1))
+    delta_time = time.time() - start_delta_time
+    print 'DONE',str(datetime.timedelta(seconds=delta_time))
+
+    bridge_indx = np.asarray(res, dtype=object)[:,0].astype('int')
+    bridge_risk_data = np.vstack(np.asarray(res, dtype=object)[:,1]).T
---
+++ bridge_ranking_preprocessing2.py
@@ -0,0 +1,115 @@
+"""
+Created on Thu Jun 04 12:19:41 2015
+
+@author: cedavidyang
+"""
+__author__ = 'cedavidyang'
+
+import os
+import sys
+import psycopg2
+import numpy as np
+import scipy.stats as stats
+import pyNBI.bridge as pybridge
+import pyNBI.traffic as pytraffic
+
+import pyDUE.generate_graph as g
+import pyDUE.ue_solver as ue
+from pyNataf.nataf import natafcurve
+from pyNataf.robust import semidefinitive
+from pyNBI.risk import social_cost, bridge_cost
+from cvxopt import matrix, mul
+
+from multiprocessing import Pool, Manager, freeze_support
+import itertools
+
+import time
+import datetime
+
+# load existing metadata
+filename = os.path.join(os.path.abspath('./'), 'Data', 'Python', 'metadata.out')
+my_shelf = shelve.open(filename, 'r')
+for key in my_shelf:
+    globals()[key]=my_shelf[key]
+my_shelf.close()
+
+
+# update according to new data
+# global variables for parallel computing... stupid multiprocessing in Python
+
+# open databases
+conn_gis = psycopg2.connect("dbname='gisdatabase' user='amadeus' host='localhost' password='19881229'")
+cur_gis = conn_gis.cursor()
+conn_nbi = psycopg2.connect("dbname='nbi' user='amadeus' host='localhost' password='19881229'")
+cur_nbi = conn_nbi.cursor()
+
+# retrieve initial condition states of bridges
+bridge_db = pytraffic.retrieve_bridge_db(cur_gis, cur_nbi)
+# get transition matrix
+if os.path.isfile('./pmatrix.npy'):
+    pmatrix = np.load('pmatrix.npy')
+else:
+    pmatrix = pybridge.transition_matrix()
+
+#create graph
+theta = matrix([0.0,0.0,0.0,0.15])
+delaytype = 'Polynomial'
+graph0 = g.LA_county(parameters=theta,delaytype=delaytype, cur_gis = cur_gis)
+nlink = len(graph0.links)
+conn_gis.close()
+conn_nbi.close()
+
+# capacity drop
+cap_drop_array = np.ones(np.asarray(bridge_db, dtype=object).shape[0])*0.1
+
+# initial capacity without failed bridges
+all_capacity = np.zeros(nlink)
+for link, link_indx in graph0.indlinks.iteritems():
+    all_capacity[link_indx] = graph0.links[link].capacity
+
+## initial delay
+#res0 = ue.solver_fw(graph0, full=True)
+#delay0 = res0[1][0,0]
+#length_vector = np.zeros(len(graph0.links.keys()))
+#for link_key, link_indx in graph0.indlinks.iteritems():
+#    length_vector[link_indx] = graph0.links[link_key].length
+#distance0  = (res0[0].T * matrix(length_vector))[0,0]
+##res_bench = ue.solver(graph0)
+
+## create bookkeeping dict
+#bookkeeping = {}
+
+## correlation
+#corr_length = 8.73
+#correlation = pybridge.bridge_correlation(bridge_db, corr_length)
+#norm_cov = semidefinitive(correlation, tol=1e-14, deftol=1e-12)
+##norm_cov = None
+#
+## with nataf
+#popt = np.load('nataf_popt.npy')
+#def nataf(x):
+#    return natafcurve(x,*popt)
+#nataf=None
+#
+#if nataf is not None:
+#    norm_cov = nataf(correlation)
+#try:
+#    tmp = np.linalg.cholesky(norm_cov)
+#except:
+#    norm_cov = semidefinitive(norm_cov, tol=1e-14, deftol=1e-12)
+
+import shelve
+filename = os.path.join(os.path.abspath('./'), 'Data', 'Python', 'metadata.out')
+my_shelf = shelve.open(filename,'n') # 'n' for new
+keys = ['bridge_db', 'all_capacity', 'cap_drop_array', 'res0', 'length_vector', 'pmatrix', 'theta',
+        'delaytype', 'graph0', 'popt', 'nataf', 'norm_cov', 'corr_length', 'delay0', 'distance0']
+for key in keys:
+    try:
+        my_shelf[key] = globals()[key]
+    except:
+        #
+        # __builtins__, my_shelf, and imported modules can not be shelved.
+        #
+        if not key.startswith("_"):
+            print('ERROR shelving: {0}'.format(key))
+my_shelf.close()
